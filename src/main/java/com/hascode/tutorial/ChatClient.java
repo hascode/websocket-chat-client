9e53a6a7b365de8bfe76adee0d7bcbcfd612005e
Micha Kops <micha.kops@gmail.com>
2014-11-05 21:04:54 +0100

    Console chat client added.

--- src/main/java/com/hascode/tutorial/ChatClient.java
+++ src/main/java/com/hascode/tutorial/ChatClient.java
@@ -1,29 +1,41 @@
 package com.hascode.tutorial;
 
+import java.io.Console;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 import javax.json.Json;
+import javax.json.JsonObject;
 
 public class ChatClient {
 	public static void main(final String[] args) throws InterruptedException, URISyntaxException {
-		System.out.println("starting chat");
-		final ChatClientEndpoint clientEndPoint = new ChatClientEndpoint(new URI("ws://0.0.0.0:8080/hascode/chat/java"));
-		clientEndPoint.addMessageHandler(message -> {
-			System.out.println("Message received: " + message);
-			clientEndPoint.sendMessage("How are you?");
+		Console console = System.console();
+		String userName = console.readLine("Please enter your user name: ");
+		String roomName = console.readLine("Please enter a chat-room name: ");
+		System.out.println("connecting to chat-room " + roomName);
+		final ChatClientEndpoint clientEndPoint = new ChatClientEndpoint(new URI("ws://0.0.0.0:8080/hascode/chat/" + roomName));
+		clientEndPoint.addMessageHandler(response -> {
+			System.out.println(jsonMessageToString(response, roomName));
 		});
 
 		while (true) {
-			String user = "fred";
-			String message = "hey there";
-			clientEndPoint.sendMessage(createJsonMessage(user, message));
+			String message = console.readLine("%s@%s: ", userName, roomName);
+			clientEndPoint.sendMessage(stringToJsonMessage(userName, message));
 			Thread.sleep(30000);
 		}
 	}
 
-	private static String createJsonMessage(final String user, final String message) {
+	private static String stringToJsonMessage(final String user, final String message) {
 		return Json.createObjectBuilder().add("sender", user).add("message", message).build().toString();
+	}
+
+	private static String jsonMessageToString(final String response, final String roomName) {
+		JsonObject root = Json.createReader(new StringReader(response)).readObject();
+		String message = root.getString("message");
+		String sender = root.getString("sender");
+		String received = root.getString("received");
+		return String.format("%s@%s: %s [%s]", sender, roomName, message, received);
 	}
 
 }
